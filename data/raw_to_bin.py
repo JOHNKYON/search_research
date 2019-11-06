a5d34adde8f45a80eda712193631c2286998b739
JOHNKYON <zhaofengbo1321@163.com>
2019-11-06 15:12:50 +0900

    Linear scan qury time recording implemented.

---
+++ data/raw_to_bin.py
@@ -0,0 +1,84 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import h5py
+import sys
+
+
+def raw_to_binary(path, encoder):
+    """
+    Create a binary file based on raw.txt file
+    in given path and a given encoding function.
+
+    The binary file would be of hdf5 format and named
+    ${len}_${size}_${querySize}.mat.
+
+    The binary file contains following information:
+
+    keys = ['B', 'Q']
+    While   B.shape = (${size}, ${len})
+            Q.shape = (${querySize}, ${len})
+
+
+    :param path:    The path to where the raw data is.
+    :return:
+    """
+
+    file_path = path + "/raw.txt"
+    with open(file_path, 'r') as file:
+        length = int(file.readline())
+        size = int(file.readline())
+        query_size = int(file.readline())
+        with h5py.File(path + "/" + str(length) + "_" + str(size) +
+                       "_" + str(query_size) +
+                       ".mat", 'w') as h5_file:
+            h5_file.create_dataset("B", shape=(size, length/2), dtype='uint8')
+            h5_file.create_dataset("Q", shape=(query_size, length/2), dtype='uint8')
+            for i in range(size):
+                h5_file["B"][i] = encoder.encode("base", file.readline()[:-1])
+            for i in range(query_size):
+                h5_file["Q"][i] = encoder.encode("query", file.readline()[:-1])
+
+
+class Encoder:
+    """
+    A class of encoder that contains a map for encoding and provide
+    encoding function
+    """
+
+    def __init__(self, base_map=None, query_map=None):
+        if base_map is None:
+            base_map = {"0": 0,
+                        "1": 3,
+                        "2": 15}
+        if query_map is None:
+            query_map = {"0": 0,
+                         "1": 5,
+                         "2": 15}
+
+        self.map = {"base": base_map,
+               "query": query_map}
+
+    def encode(self, name , string):
+        length = len(string)
+        if length % 2 != 0:
+            print("Warning! The encoding length would not fill an 8 bit integer!")
+        array = []
+        a = 0
+        for i in range(length):
+            a += self.map[name][string[i]]
+            if (i & 1) == 0:
+                a <<= 4
+            else:
+                array.append(a)
+                a = 0
+        return array
+
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    assert len(argv) == 1
+
+    encoder = Encoder()
+    raw_to_binary(argv[0], encoder)
---
+++ data/validation.py
@@ -0,0 +1,120 @@
+import heapq
+import h5py
+import sys
+import time
+
+
+def linear_scan(path, query, k):
+    """
+    This function do linear scan kNN search on the raw data in the
+    given path, a given query and a given k.
+
+    >>> linear_scan("data/data/4_6_2", "0000", 2)
+    [0,1]
+
+    :param path:
+    :param query:
+    :param k:
+    :return:
+    """
+    file_path = path + "/raw.txt"
+    candidates = []
+    with open(file_path, 'r') as file:
+        length = int(file.readline())
+        data_size = int(file.readline())
+        query_size = int(file.readline())
+        for i in range(data_size):
+            string = file.readline().rstrip()
+            dist = hamming_distance(query, string)
+            if len(candidates) < k:
+                heapq.heappush(candidates, [dist, i])
+
+            elif len(candidates) >= k and dist > candidates[0][0]:
+                heapq.heappop(candidates)
+                heapq.heappush(candidates, [dist, i])
+
+    idx = []
+    for candidate in candidates:
+        idx.append(candidate[1])
+
+    return idx
+
+
+def hamming_distance(a, b):
+    """
+    >>> hamming_distance("0012", "0111")
+    -2
+
+    :param a:
+    :param b:
+    :return:
+    """
+    assert len(a) == len(b)
+
+    count = 0
+    for i in range(len(a)):
+        count = count+1 if a[i] != b[i] else count
+    return -count
+
+
+def compare_results(res1, res2):
+    """
+    >>> compare_results([[1,3,2],[0,9,3]], [[3,1,2], [3,0,9]])
+    True
+
+    >>> compare_results([[1,3,2],[0,9,1]], [[3,1,2], [3,0,9]])
+    False
+
+    :param res1:
+    :param res2:
+    :return:
+    """
+
+    assert len(res1) == len(res2)
+
+    for i in range(len(res1)):
+        temp1 = sorted(res1[i])
+        temp2 = sorted(res2[i])
+        for j in range(len(temp1)):
+            if temp1[j] != temp2[j]:
+                return False
+
+    return True
+
+
+def load_query(path):
+    file_path = path + "/raw.txt"
+    query = []
+    with open(file_path, 'r') as file:
+        length = int(file.readline())
+        data_size = int(file.readline())
+        query_size = int(file.readline())
+        for _ in range(data_size):
+            file.readline()
+        for _ in range(query_size):
+            query.append(file.readline().rstrip())
+    return query
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    path = args[0]
+    k = int(args[1])
+
+    # Linear Scan
+    queries = load_query(path)
+    linscan_results = []
+    time1 = time.perf_counter()
+    for query in queries:
+        linscan_results.append(linear_scan(path, query, k))
+    query_time = time.perf_counter()
+    with open(path+"/linear_query_time.txt", 'w') as f:
+        f.write(str(query_time))
+
+    # Validation
+    file_name = path.split('/')[-1]
+
+    # TODO: Change this to mih
+    # TODO: Check for validation
+    with h5py.File(path + '/mih_' + file_name + '.h5', 'r') as h5_file:
+        print(compare_results(h5_file['refs']['linscan0.res'], linscan_results))

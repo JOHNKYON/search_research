fe480deecfb814cedc282a18f8e9da20dca68801
JOHNKYON <zhaofengbo1321@163.com>
2019-11-11 18:15:15 +0900

    Improve data generation strategy.

--- data/data_generation.py
+++ data/data_generation.py
@@ -28,6 +28,53 @@ def raw_data_generation(length, size, query_size):
             file.write(build_string(length) + '\n')
 
 
+def build_string_p(length, p):
+    """
+    This function works similar to build_string_p
+
+    It build a string with length ${length},
+
+    For each character, it obeys the following rules:
+        1 with possibility of 2p.
+        2 with possibility of p^2.
+        Otherwise 0.
+    :param length:
+    :param p:
+    :return:
+    """
+    assert 0.0 < p < 1.0
+    s = ""
+    for _ in range(length):
+        r = random.uniform(0, 1)
+        if 0.0 < r < pow(p, 2):
+            s += "2"
+        elif 1.0 - pow((1.0-p), 2) < r < 1.0:
+            s += "0"
+        else:
+            s += "1"
+    return s
+
+
+
+def raw_data_generation_r(length, size, query_size, p):
+    """
+    This function functions similarly to raw_data_generation()
+    Generate data with a given possibility p.
+
+    1 is generated with possibility 2p.
+    2 is generated with possibility p^2.
+    Otherwise 0.
+    :return:
+    """
+    path = "data/" + str(length) + "_" + str(size) + "_" + str(query_size) + "_p" + str(p)
+    os.makedirs(path)
+    file_path = path + "/raw.txt"
+    with open(file_path, 'a') as file:
+        file.write(str(length) + '\n' + str(size) + '\n' + str(query_size) + '\n')
+        for _ in range(size + query_size):
+            file.write(build_string_p(length, p) + '\n')
+
+
 def build_string(length):
     """
     Build a string of length len, with alphabet of {0, 1, 2}
@@ -43,6 +90,11 @@ def build_string(length):
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    assert len(argv) == 3
 
-    raw_data_generation(int(argv[0]), int(argv[1]), int(argv[2]))
+    assert len(argv) == 3 or len(argv) == 4
+
+    if len(argv)  == 3:
+        raw_data_generation(int(argv[0]), int(argv[1]), int(argv[2]))
+    else:
+        raw_data_generation_r(int(argv[0]), int(argv[1]), int(argv[2]), float(argv[3]))
+

f22f865bfcbda9f13cc00c6b1d2dbacc7e129816
JOHNKYON <zhaofengbo1321@163.com>
2019-12-17 18:16:04 +0900

    Implement heuristic compare results script.

--- data/gather_results/heuristic_compare.py
+++ data/gather_results/heuristic_compare.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def compare_reults(csv_file, h5_file):
+def compare_results(csv_path, h5_path):
     """
     Compare two files that contains their searching results.
     Record the percentage of same results for different K.
@@ -13,12 +13,42 @@ def compare_reults(csv_file, h5_file):
     :param h5_file:
     :return:
     """
-    pass
+    acc_res = pd.read_csv(csv_path).to_numpy()
+    heur_res = get_heuristic_res(h5_path)
+
+    assert acc_res.shape == heur_res.shape
+
+    k = 10
+    res = {}
+    q_count = acc_res.shape[0]
+    while k <= 100000:
+        res[k] = 0.0
+        for idx in range(q_count):
+            res[k] += np.intersect1d(acc_res[idx], heur_res[idx]).shape[0] / k
+
+    return res
+
+
+
+def get_heuristic_res(h5_path):
+    """
+    Return a numpy array contains heuristic search results
+    from a h5 file
+
+    :param h5_path:
+    :return:
+    """
+    with h5py.File(h5_path, 'r') as file:
+        return file_path[file['refs']['mih0.res']].value
+
+
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
     file_path = argv[:2]
     out_path = argv[2]
 
-    result = compare_retuls(file_path[0], file_path[1])
+    result = compare_results(file_path[0], file_path[1])
+
+    pd.DataFrame(result).to_csv(out_path)
 
